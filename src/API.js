47974d521bcd4db68c82b29482fc49e2c6a86450
OOlleennaa <123268201+OOlleennaa@users.noreply.github.com>
2024-01-02 22:16:27 +0000

    bingo!

---
+++ src/API.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const API_KEY = '38417070-f95938a425bab398a492a74ff';
+axios.defaults.baseURL = `https://pixabay.com/api/`;
+
+export const fetchImages = async (query, page) => {
+  const separated = query.split('/');
+  const exstractedQuery = separated[1];
+  try {
+    const response = await axios.get('', {
+      params: {
+        key: API_KEY,
+        q: exstractedQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        page: page,
+        per_page: 12,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
